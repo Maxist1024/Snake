3516798b3d871e26f43c56c4726f369cb2d150cc
Maxist1024 <duda.karol1@gmail.com>
2018-05-02 13:31:28 +0200

    Polaczenie klienta z serwerem

--- Snake.UI/ViewModels/StartUpViewModel.cs
+++ Snake.UI/ViewModels/StartUpViewModel.cs
@@ -1,16 +1,17 @@
 ﻿using Prism.Commands;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 using System.Windows;
 using System.Windows.Input;
 
 namespace Snake.UI.ViewModels
 {
-    public class StartUpViewModel:ViewModelBase
+    public class StartUpViewModel : ViewModelBase
     {
+        byte[] bytes = new byte[1024];
+        Socket senderSock;
+
         public StartUpViewModel()
         {
             PlayGameCommand = new DelegateCommand<Window>(OnPlayGameExecute);
@@ -20,13 +21,49 @@ namespace Snake.UI.ViewModels
 
         private void OnPlayGameExecute(Window window)
         {
-            MainWindow mainWindow = new MainWindow();
-            mainWindow.Show();
-            
-            if (window != null)
+            try
             {
-                window.Close();
+                // Create one SocketPermission for socket access restrictions 
+                SocketPermission permission = new SocketPermission(
+                    NetworkAccess.Connect,    // Connection permission 
+                    TransportType.Tcp,        // Defines transport types 
+                    "",                       // Gets the IP addresses 
+                    SocketPermission.AllPorts // All ports 
+                    );
+
+                // Ensures the code to have permission to access a Socket 
+                permission.Demand();
+
+                // Resolves a host name to an IPHostEntry instance            
+                IPHostEntry ipHost = Dns.GetHostEntry("");
+
+                // Gets first IP address associated with a localhost 
+                IPAddress ipAddr = ipHost.AddressList[0];
+
+                // Creates a network endpoint 
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 4510);
+
+                // Create one Socket object to setup Tcp connection 
+                senderSock = new Socket(
+                    ipAddr.AddressFamily,// Specifies the addressing scheme 
+                    SocketType.Stream,   // The type of socket  
+                    ProtocolType.Tcp     // Specifies the protocols  
+                    );
+
+                senderSock.NoDelay = false;   // Using the Nagle algorithm 
+
+                // Establishes a connection to a remote host 
+                senderSock.Connect(ipEndPoint);
+                MessageBox.Show("Socket connected to " + senderSock.RemoteEndPoint.ToString());
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+
+                if (window != null)
+                    window.Close();
             }
+            catch (Exception exc) { MessageBox.Show(exc.ToString()); }
+
+
         }
     }
 }
